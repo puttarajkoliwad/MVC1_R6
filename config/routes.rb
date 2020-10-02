249b1f003a0c0566cee139e008d615e73e97a23f
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-10-02 15:34:11 +0530

    Create Users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   #rails routes --expanded: This command lists all the routes defined in the application in user-friendly way.
 
   resources :articles
